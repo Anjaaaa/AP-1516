60419f50a9b23237d0e0435eaf7761c53b29cff8
Clara Rittmann <clara.rittmann@googlemail.com>
2015-12-11 17:43:28 +0100

    Auswertung geschrieben, Ideen Diskussion

--- V301-EMK und Innenwiderstand/Innenwiderstand.py
+++ V301-EMK und Innenwiderstand/Innenwiderstand.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
+from tabulate import tabulate
 
 I_b, U_b = np.genfromtxt('Messungb.txt', unpack=True)
 I_c, U_c = np.genfromtxt('Messungc.txt', unpack=True)
@@ -25,8 +26,8 @@ regression_b = np.poly1d(fit)
 R_i = parameters[0] # für Aufgabe 4c,d)
 Uo = parameters[1] #für Aufgabe 4d)
 
-print('Leerlaufspannung Messung B:', parameters[1])
-print('Innenwiderstand Messung B:', parameters[0])
+print('Leerlaufspannung Messung B:', parameters[1], '+/-', np.sqrt(popt[1,1]))
+print('Innenwiderstand Messung B:', parameters[0], '+/-', np.sqrt(popt[0,0]))
 x = np.linspace(0.00,0.3)
 plt.plot(x, regression_b(x), 'b-', label='Regressionsgerade')
 plt.errorbar(I_b, U_b, xerr=I_b*0.03, yerr=U_b*0.02, fmt='r.', label='Datenpunkte')
@@ -43,8 +44,8 @@ parameters, popt = curve_fit(f, I_c, U_c)
 fit = np.polyfit(I_c, U_c, 1)
 regression_c = np.poly1d(fit)
 
-print('Leerlaufspannung Messung c:', parameters[1])
-print('Innenwiderstand Messung c:', parameters[0])
+print('Leerlaufspannung Messung c:', parameters[1], '+/-', np.sqrt(popt[1,1]))
+print('Innenwiderstand Messung c:', parameters[0], '+/-', np.sqrt(popt[0,0]))
 
 y = np.linspace(0.01,0.4)
 plt.plot(y, regression_c(y), 'b-', label = 'Regressionsgerade')
@@ -63,14 +64,16 @@ parameters, popt = curve_fit(f, I_d, U_d)
 fit = np.polyfit(I_d, U_d, 1)
 regression_d = np.poly1d(fit)
 
-print('Leerlaufspannung Messung d (Rechteck):', parameters[1])
-print('Innenwiderstand Messung d', parameters[0]) #Achtung I in mA gemessen
+print('Leerlaufspannung Messung d (Rechteck):', parameters[1], '+/-', np.sqrt(popt[1,1]))
+print('Innenwiderstand Messung d', parameters[0], '+/-', np.sqrt(popt[0,0])) #Achtung I in mA gemessen
 
-plt.plot(I_d, regression_d(I_d), 'b-', label='Regressionsgerade')
+z = np.linspace(0.001,0.007)
+plt.plot(z, regression_d(z), 'b-', label='Regressionsgerade')
 plt.errorbar(I_d, U_d, xerr=I_d*0.03, yerr=U_d*0.02, fmt='r.', label='Datenpunkte')
 plt.xlabel('Stromstärke / A')
 plt.ylabel('Spannung / V')
 plt.legend(loc='best')
+plt.xlim(0.001,0.007)
 plt.savefig('Spannung_Messung_d.pdf')
 plt.show()
 
@@ -81,13 +84,16 @@ parameters, popt = curve_fit(f, I_e, U_e)
 fit = np.polyfit(I_e, U_e, 1)
 regression_e = np.poly1d(fit)
 
-print('Leerlaufspannung Messung e (Sinus):', parameters[1])
-print('Innenwiderstand Messung e:', parameters[0])
+print('Leerlaufspannung Messung e (Sinus):', parameters[1], '+/-', np.sqrt(popt[1,1]))
+print('Innenwiderstand Messung e:', parameters[0], '+/-', np.sqrt(popt[0,0]))
 
-plt.plot(I_e, regression_e(I_e), 'b-', label ='Regressionsgerade')
+
+q = np.linspace(0,0.00035)
+plt.plot(q, regression_e(q), 'b-', label ='Regressionsgerade')
 plt.errorbar(I_e, U_e, xerr=I_e*0.03, yerr=U_e*0.02, fmt='r.', label = 'Datenpunkte')
 plt.xlabel('Stromstärke / A')
 plt.ylabel('Spannung / V')
+plt.xlim(0,0.00035)
 plt.legend(loc='best')
 plt.savefig('Spannung_Messung_e.pdf')
 plt.show()
@@ -106,13 +112,20 @@ U_b = unp.uarray(U_b, U_b*0.02)
 Leistung = I_b * U_b
 Belastungswiderstand = U_b / I_b
 
+tabelle = np.array([unp.nominal_values(Belastungswiderstand), unp.std_devs(Belastungswiderstand), unp.nominal_values(Leistung), unp.std_devs(Leistung)])
+
+f = open('tabelle.txt', 'w')
+f.write(tabulate(tabelle.T, tablefmt="latex"))
+
+
+
 x = np.linspace(0,60, 100)
 N = (Uo / (x + R_i))**2 * x # N=Leistung
 
 for i in range (0, 10):
-    print(unp.nominal_values(Leistung[i]), unp.std_devs(Leistung[i]))
+    print(unp.nominal_values(Leistung[i]), unp.std_devs(Leistung))
 
-plt.errorbar(unp.nominal_values(Belastungswiderstand), unp.nominal_values(Leistung), yerr = unp.std_devs(Leistung),fmt = 'rx', label='Leistung aus Messdaten')
+plt.errorbar(unp.nominal_values(Belastungswiderstand), unp.nominal_values(Leistung), xerr = unp.std_devs(Belastungswiderstand), yerr = unp.std_devs(Leistung),fmt = 'r.', label='Leistung aus Messdaten')
 plt.plot(x, N, 'g-', label = 'Errechnete Leistungskurve')
 plt.xlabel('Lastwiderstand / $\Omega$')
 plt.ylabel('Leistung / W')
