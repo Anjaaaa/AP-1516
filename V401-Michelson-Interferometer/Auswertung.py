573622626b24dbd56e2db131d4265776b5e6150b
Clara Rittmann <clara.rittmann@googlemail.com>
2016-04-20 21:55:04 +0200

    .

--- V401-Michelson-Interferometer/Auswertung.py
+++ V401-Michelson-Interferometer/Auswertung.py
@@ -40,23 +40,27 @@ Wellenlange_gesamt = ufloat(Wellenlange_Mittel, Wellenlange_Fehler)
 print(Wellenlange_gesamt)
 
 
-write('build/Wellenlange_gesamt.tex', make_SI(Wellenlange_gesamt,r'\meter','e-12', figures=4))
+write('build/Wellenlange_gesamt.tex', make_SI(Wellenlange_gesamt,r'\meter','e-9', figures=4))
 
 
 # Brechungsindex berechnen
 
-b = 5e-3 #Kammernbreite in meter
+b = 5*10**-2 #Kammernbreite in meter
 T = 293.15 # Raumtemperatur in Kelvin
 T_0 = 273.15 # Normaltemperatur in Kelvin
-p_0 = 1.10132 # Normaldruck in bar
+p_0 = 1.01325 # Normaldruck in bar
 
-deltan_Luft = Wellenlange_gesamt *10**-12 * Impuls_Luft / 2 / b
+deltan_Luft = Wellenlange_gesamt *10**-9 * Impuls_Luft / 2 / b
 n_Luft = 1 + deltan_Luft * (T/T_0)*(p_0/np.abs(Druck_Luft))
 
-write('build/Tabelle4.tex', make_table([np.abs(Druck_Luft), Impuls_Luft, unp.nominal_values(n_Luft), unp.std_devs(n_Luft)],[1,0,8,8]))
+write('build/Tabelle4.tex', make_table([np.abs(Druck_Luft), Impuls_Luft, unp.nominal_values(n_Luft), unp.std_devs(n_Luft)],[1,0,6,6]))
 
-deltan_CO2 = Wellenlange_gesamt *10**-12 * Impuls_CO2 / 2 / b
+deltan_CO2 = Wellenlange_gesamt *10**-9 * Impuls_CO2 / 2 / b
 n_CO2 = 1 + deltan_CO2 * (T/T_0)*(p_0/np.abs(Druck_CO2))
+
+write('build/Tabelle5.tex', make_table([np.abs(Druck_Luft), Impuls_Luft, unp.nominal_values(n_Luft)],[1,0,5]))
+
+write('build/Tabelle6.tex', make_table([np.abs(Druck_CO2), Impuls_CO2, unp.nominal_values(n_CO2)],[1,0,5]))
 
 print(n_Luft)
 print(unp.nominal_values(n_Luft))
