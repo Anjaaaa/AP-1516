ed431bf2b5d38645a1fafbfc5675fc3f666fad1e
Clara Rittmann <clara.rittmann@googlemail.com>
2016-05-08 13:08:43 +0200

    Auswertung und Diskussion fertig

--- V500-Der-Photoeffekt/Rechnung2.py
+++ V500-Der-Photoeffekt/Rechnung2.py
@@ -21,6 +21,7 @@ write('build/tabelle_orange_wellenlange.tex', make_table([spannung_orange, orang
 
 #pikoampere in ampere umrechnen
 matrix = matrix*10**-12
+orange = orange*10**-12
 
 
 
@@ -34,14 +35,14 @@ for j in range(0,5):
 			
 
 #wellenlangen plotten
-plt.plot(spannung, np.sqrt(matrix_plus[0,:]), 'ro', label='rot')
-plt.plot(spannung, np.sqrt(matrix_plus[1,:]), 'go', label='grün')
-plt.plot(spannung, np.sqrt(matrix_plus[2,:]), 'mo', label='lila')
-plt.plot(spannung, np.sqrt(matrix_plus[3,:]), 'bo', label='blau')
-plt.plot(spannung, np.sqrt(matrix_plus[4,:]), 'ko', label='UV')
+plt.plot(spannung, 10**6*np.sqrt(matrix_plus[0,:]), 'ro', label='rot')
+plt.plot(spannung, 10**6*np.sqrt(matrix_plus[1,:]), 'go', label='grün')
+plt.plot(spannung, 10**6*np.sqrt(matrix_plus[2,:]), 'mo', label='lila')
+plt.plot(spannung, 10**6*np.sqrt(matrix_plus[3,:]), 'bo', label='blau')
+plt.plot(spannung, 10**6*np.sqrt(matrix_plus[4,:]), 'ko', label='UV')
 
 plt.xlim(-2,2)
-plt.ylabel(r'Wurzel des Stromes / $\sqrt{\mathrm{I}}$')
+plt.ylabel(r'Wurzel des Stromes $10^{-6}$ / $\sqrt{\mathrm{I}}$')
 plt.xlabel(r'Spannung / V')
 plt.legend(loc='best')
 plt.savefig('build/AlleWellenlangen.png')
@@ -64,17 +65,19 @@ for i in range (0,5):
 	U_gegen[i] = Ug.n
 	#plots erstellen
 	x = np.linspace(-2, 2)
-	plt.plot(x, g(x, *parameters), 'k-')
-	plt.plot(spannung, np.sqrt(matrix_plus[i,:]), 'rx')
+	null = np.zeros(50) #da linspace 50 werte erzeugt
+	plt.plot(x, 10**6*g(x, *parameters), 'k-')
+	plt.plot(spannung, 10**6*np.sqrt(matrix_plus[i,:]), 'rx')
+	plt.plot(x, null, 'k-')
 	plt.xlim(-2,2)
-	plt.ylabel(r'Wurzel des Stromes / $\sqrt{\mathrm{I}}$')
+	plt.ylabel(r'Wurzel des Stromes $10^{-6}$ / $\sqrt{\mathrm{I}}$')
 	plt.xlabel(r'Spannung / V')
 	plt.savefig('build/regression_Farbe:'+str(i)+'.png')
 	plt.show()
 	#daten speichern
-	write('build/Steigung_'+str(i)+'.tex', make_SI(m, r'\sqrt{\ampere}\per\volt', figures=5))
-	write('build/y-Achsenabschnitt_'+str(i)+'.tex', make_SI(b, r'\sqrt{\ampere}', figures=5))
-	write('build/Grenzspannung_'+str(i)+'.tex', make_SI(Ug, r'\volt', figures=5))
+	write('build/Steigung_'+str(i)+'.tex', make_SI(m, r'\sqrt{\ampere}\per\volt', figures=2))
+	write('build/y-Achsenabschnitt_'+str(i)+'.tex', make_SI(b, r'\sqrt{\ampere}', figures=2))
+	write('build/Grenzspannung_'+str(i)+'.tex', make_SI(Ug, r'\volt', figures=2))
 		
 
 
@@ -97,14 +100,14 @@ for i in range(0,5):
 
 # in frequenz (1/s) umrechnen:
 c = 298792458 #Lichtgeschwindgkeit in m/s
-n= c/(L*10**-12)
+n= c/(L*10**-9)
 
 def  f(n, m, b):
     		return m*(n) + b
     		
 parameters, popt = curve_fit(f, n, U_gegen)
 
-x = np.linspace(4.5e17, 8.5e17)
+x = np.linspace(4.5e14, 8.5e14)
 plt.plot(x, f(x, *parameters), 'k-')
 plt.plot(n, U_gegen, 'rx')
 #plt.xlim(-2,2)
@@ -113,7 +116,32 @@ plt.xlabel(r'Frequenz / $\nu$')
 plt.savefig('build/regression_aufgabe2.png')
 plt.show()
 
-# ich denke um die austrittsarbeit zu berechnen, muss ich elementarladung eines elektrons nachschauen :(
+#Placksches wisckungsquantum durch elementarladung
+write('build/h_durch_e.tex', make_SI(ufloat(parameters[0],np.sqrt(popt[0,0]))*10**15, r'\volt\second','e-15', figures=2))
+
+write('build/h_durch_e_2.tex', make_SI(ufloat(parameters[0],np.sqrt(popt[0,0]))*10**15, r'\electronvolt\second','e-15', figures=2))
+
+write('build/austrittsarbeit.tex', make_SI(ufloat(parameters[1],np.sqrt(popt[1,1])), r'\volt', figures=2))
+
+write('build/austrittsarbeit_2.tex', make_SI(-ufloat(parameters[1],np.sqrt(popt[1,1])), r'\electronvolt', figures=2))
+
+
+write('build/tabelle_h_druch_e.tex', make_table([U_gegen, L, n*10**-12],[2,0,0]))
+
+
+
+
+# Aufgabe 3, plotten der orangenen skektrallinie
+
+x=np.linspace(-20,20)
+plt.plot(spannung_orange, 10**12*orange, 'bx')
+plt.xlim(-20,20)
+plt.ylabel(r'Photoenstrom / pA')
+plt.plot(x, np.zeros(50), 'k-')
+plt.xlabel(r'Spannung / V')
+plt.savefig('build/OrangeWellenlange.png')
+plt.show()
+
 
 
     	
