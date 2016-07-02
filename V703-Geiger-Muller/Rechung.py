3240e71481254c7aec877fb45f60611f6320e6dd
Clara Rittmann <clara.rittmann@googlemail.com>
2016-07-02 20:13:06 +0200

    .

--- V703-Geiger-Muller/Rechung.py
+++ V703-Geiger-Muller/Rechung.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
+from scipy.constants import e
 from table import (
     make_table,
     make_full_table,
@@ -17,7 +18,7 @@ Z_fehler = np.sqrt(c)/t
 write('build/tabelle_charakteristik.txt', make_table([U[:15], Z[:15], Z_fehler[:15], U[15:], Z[15:], Z_fehler[15:]], [2,2,2,2,2,2]))
 
 plt.plot(U, Z, 'ko', label='Messwerte')
-plt.errorbar(U, Z, xerr=1, yerr=Z_fehler, fmt='r.', label = r'Fehler $\sqrt{n}$')
+plt.errorbar(U, Z, xerr=1, yerr=Z_fehler, fmt='r.', label = r'Statistischer Fehler')
 plt.legend(loc='best')
 plt.xlabel(r'Spannung $U \ /\  \mathrm{V}$') 
 plt.ylabel(r'Zählrate $Z \ /\ {\mathrm{Counts}}/{\mathrm{s}}$')
@@ -44,7 +45,7 @@ x = np.linspace(300,750)
 plt.plot(U[12:], Z[12:], 'ro', label='Messwerte')
 plt.plot(U[15:27], Z[15:27], 'ko', label='Messwerte (linearer Teil)')
 plt.plot(x, linear(x, *parameters1),'k-', label = 'Regression (linearer Teil)')
-plt.errorbar(U[12:], Z[12:], xerr=1, yerr=Z_fehler[12:], fmt='r.', label = r'Fehler $\sqrt{n}$')
+plt.errorbar(U[12:], Z[12:], xerr=1, yerr=Z_fehler[12:], fmt='r.', label = r'Statistischer Fehler')
 plt.legend(loc='best')
 plt.xlabel(r'Spannung $U \ /\  \mathrm{V}$') 
 plt.ylabel(r'Zählrate $Z \ /\ {\mathrm{Counts}}/{\mathrm{s}}$')
@@ -75,6 +76,38 @@ write('build/Z2.txt', make_SI(Z2, r'\per\second', figures=1))
 
 write('build/s.txt', make_SI(s, r'', figures=1))
 #write('build/tabelle_messung1.txt', make_table([p1, puls1, x1], [0,0,2]))
+
+
+
+############# totzeit über 2-präparat-methode
+N1 = ufloat(14866 / 60, np.sqrt(14866)/60)
+N12 = ufloat(31548 / 60, np.sqrt(31548) / 60)
+N2 = ufloat(17281 / 60, np.sqrt(17281) / 60)
+
+
+totzeit = (N1 + N2 - N12)/(2*N1*N2)
+print(totzeit)
+write('build/totzeit.txt', make_SI(totzeit*1e6, r'\micro\second', figures=1))
+
+
+
+############### ladungen pro impuls berechnen:
+spannung, counts, strom = np.genfromtxt('Messung2.txt', unpack = True)
+strom = strom * 1e-6 #mircoampere in ampere
+
+#counts = ufloat(counts, np.sqrt(counts))
+
+Q = strom / counts * 10 #landung pro count
+
+Q = Q/e
+
+write('build/tabelle_ladungen.txt', make_table([spannung, counts, strom*1e6, Q*1e-9], [0,0,2, 2]))
+
+print(Q)
+
+
+
+
 
 
 
